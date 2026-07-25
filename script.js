ec94cd13b590ae84612a7d5b971f46cc5774188f
Mohd Azam | Full Stack Developer <azamraza7037@gmail.com>
2026-07-25 11:38:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -163,3 +163,22 @@ window.addEventListener("scroll", () => {
     });
 
 });
+/* =========================================
+   NAVBAR SCROLL EFFECT
+========================================= */
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 80) {
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
